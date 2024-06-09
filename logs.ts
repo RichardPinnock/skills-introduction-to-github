f2d88e2dda8dbc7d103fae0a2dca5b4b7c7d656d
Richard Pinnock <130163795+RichardPinnock@users.noreply.github.com>
2024-06-09 02:26:09 -0400

    🧾 Create Logs structure & types
    co-authored logs feature commit

---
+++ logs.ts
@@ -0,0 +1,25 @@
+// Console logs structures and types.
+
+
+console.log('Hello, world!');
+console.log('This is a console log.');
+console.log('Here is a number:', 42);
+console.log('Here is an object:', { foo: 'bar' });
+console.log('Here is an array:', [1, 2, 3]);
+console.log('Here is a boolean:', true);
+console.log('Here is a string:', 'Hello, world!');
+console.log('Here is a null:', null);
+console.log('Here is an undefined:', undefined);
+console.log('Here is a symbol:', Symbol('foo'));
+console.log('Here is a function:', function foo() { return 'bar'; });
+console.log('Here is a date:', new Date());
+console.log('Here is an error:', new Error('This is an error message.'));
+console.log('Here is a promise:', Promise.resolve('This is a promise.'));
+console.log('Here is a regular expression:', /foo/);
+console.log('Here is a map:', new Map([['foo', 'bar']]));
+console.log('Here is a set:', new Set(['foo', 'bar']));
+console.log('Here is a weak map:', new WeakMap());
+console.log('Here is a weak set:', new WeakSet());
+console.log('Here is an array buffer:', new ArrayBuffer(8));
+console.log('Here is a data view:', new DataView(new ArrayBuffer(8)));
+console.log('Here is a typed array:', new Int8Array(8));
